8c833677ce9240b4892d0622c33c0a11ec6214b1
YangMr <759168442@qq.com>
2023-08-24 09:06:19 +0800

    feat(global): 调整目录结构

---
+++ src/views/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="row"></div>
+  </div>
+</template>
+<style lang="scss">
+.container {
+  width: 1000px;
+  height: 500px;
+  background: yellow;
+
+  .row {
+    width: 200px;
+    height: 200px;
+    background: red;
+  }
+}
+</style>
