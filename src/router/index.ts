3b22b1fc004ad3b319abc0256afec4b3a182922b
YangMr <759168442@qq.com>
2023-08-25 09:45:41 +0800

    feat(vant): 配置vant按需加载以及css主题

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Index from '@/views/index.vue'
+
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'index',
-      component: Index
-    },
-    {
       path: '/login',
       name: 'login',
-      component: () => import('../views/Login.vue')
+      component: () => import('../views/Login/index.vue')
     }
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   component: () => import('../views/AboutView.vue')
-    // }
   ]
 })
 
---
+++ src/views/Login/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="login">
+    <a href="#">百度</a>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="success">成功按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+a {
+  background: var(--a);
+}
+</style>
