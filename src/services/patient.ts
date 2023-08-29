48103715a283637e07b61926f681aeef4a91db64
YangMr <759168442@qq.com>
2023-08-29 17:01:23 +0800

    feat(patient): 实现患者的添加/编辑/删除

--- src/services/patient.ts
+++ src/services/patient.ts
@@ -1,7 +1,22 @@
 import request from '@/utils/request'
-import type { PatientList } from '@/types/user'
+import type { PatientList, PatientType } from '@/types/user'
 
 // 查询患者列表信息
 export const getPatientList = () => {
   return request<PatientList>('/patient/mylist', 'GET')
+}
+
+// 添加患者
+export const addPatient = (patient: PatientType) => {
+  return request('/patient/add', 'POST', patient)
+}
+
+// 编辑患者
+export const editPatient = (patient: PatientType) => {
+  return request('/patient/update', 'PUT', patient)
+}
+
+// 删除患者
+export const delPatient = (id: number | string) => {
+  return request(`/patient/del/${id}`, 'DELETE')
 }
--- src/views/User/PatientPage.vue
+++ src/views/User/PatientPage.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { getPatientList } from '@/services/patient'
+import { getPatientList, addPatient, editPatient, delPatient } from '@/services/patient'
 import type { PatientList, PatientType } from '@/types/user'
 import { ref, computed } from 'vue'
 import { showToast } from 'vant'
 import Validator from 'id-validator'
+import { showConfirmDialog } from 'vant'
 
 // 创建一个变量,保存患者列表
 const list = ref<PatientList>()
@@ -25,24 +26,36 @@ const options = [
 const show = ref(false)
 
 // 打开弹出层
-const showPopup = () => {
+const showPopup = (item?: PatientType) => {
+  console.log('item', item)
+  if (item) {
+    const { id, gender, name, idCard, defaultFlag } = item
+    patient.value = { id, gender, name, idCard, defaultFlag }
+  } else {
+    patient.value = { ...initPatient }
+  }
   show.value = true
 }
 
 // 关闭弹出层
 const backPopup = () => {
-  patient.value = { ...initPatient }
   show.value = false
 }
 
 // 监听保存按钮
-const submit = () => {
+const submit = async () => {
   if (!patient.value.name) return showToast('请输入真实姓名')
   if (!patient.value.idCard) return showToast('请输入身份证号')
   const validator = new Validator()
   if (!validator.isValid(patient.value.idCard)) return showToast('身份证格式错误')
   const { sex } = validator.getInfo(patient.value.idCard)
   if (patient.value.gender !== sex) return showToast('性别和身份证不符')
+
+  // 保存/编辑患者
+  patient.value.id ? await editPatient(patient.value) : await addPatient(patient.value)
+  show.value = false
+  initPatientList()
+  showToast(patient.value.id ? '编辑成功' : '添加成功')
 }
 
 const initPatient: PatientType = {
@@ -64,6 +77,20 @@ const defaultFlag = computed({
     patient.value.defaultFlag = value ? 1 : 0
   }
 })
+
+// 删除患者
+const remove = async () => {
+  if (patient.value.id) {
+    await showConfirmDialog({
+      title: '温馨提示',
+      message: `您确认要删除 ${patient.value.name} 患者信息吗 `
+    })
+    await delPatient(patient.value.id)
+    show.value = false
+    initPatientList()
+    showToast('删除成功')
+  }
+}
 </script>
 
 <template>
@@ -77,11 +104,11 @@ const defaultFlag = computed({
           <span class="sex">{{ item.gender ? '男' : '女' }}</span>
           <span class="age">{{ item.age }}岁</span>
         </div>
-        <div class="icon"><cp-icons name="user-edit"></cp-icons></div>
+        <div class="icon" @click="showPopup(item)"><cp-icons name="user-edit"></cp-icons></div>
         <div class="tag" v-if="item.defaultFlag === 1">默认</div>
       </div>
 
-      <div class="patient-add" @click="showPopup" v-if="list && list?.length <= 6">
+      <div class="patient-add" @click="showPopup()" v-if="list && list?.length <= 6">
         <cp-icons name="user-add"></cp-icons>
         <p>添加患者</p>
       </div>
@@ -92,7 +119,7 @@ const defaultFlag = computed({
     <van-popup v-model:show="show" position="right">
       <cp-nav-bar
         :back="backPopup"
-        title="添加患者"
+        :title="patient.id ? '编辑患者' : '添加患者'"
         rightText="保存"
         @click-right="submit"
       ></cp-nav-bar>
@@ -111,6 +138,10 @@ const defaultFlag = computed({
           </template>
         </van-field>
       </van-form>
+
+      <van-action-bar>
+        <van-action-bar-button @click="remove">删除</van-action-bar-button>
+      </van-action-bar>
     </van-popup>
   </div>
 </template>
@@ -203,6 +234,16 @@ const defaultFlag = computed({
 
       .van-form {
         padding-top: 46px;
+      }
+
+      // 底部操作栏
+      .van-action-bar {
+        padding: 0 10px;
+        margin-bottom: 10px;
+        .van-button {
+          color: var(--cp-price);
+          background-color: var(--cp-bg);
+        }
       }
     }
   }
